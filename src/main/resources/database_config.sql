247e3e12c6415e145128a08d9857713fe04e3d34
MikiKru <michal_kruczkowski@o2.pl>
2020-11-23 11:02:07 +0100

    spring d1.2

---
+++ src/main/resources/database_config.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE spring_micro;
+-- utworzenie użytkownika DB
+CREATE USER 'spring_micro_user'@'localhost' identified by 'qwe123';
+-- przypisanie uprawnień dla użytkownika
+GRANT 
+	CREATE, ALTER, DROP, SELECT, INSERT, UPDATE, DELETE, REFERENCES
+ON 
+	spring_micro.*
+TO 
+	'spring_micro_user'@'localhost';
