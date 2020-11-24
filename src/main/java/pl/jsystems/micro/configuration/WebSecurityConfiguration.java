07cc597ae122d3f57390c9668eed4ec598f939b2
MikiKru <michal_kruczkowski@o2.pl>
2020-11-24 15:18:38 +0100

    spring d2.13

--- src/main/java/pl/jsystems/micro/configuration/WebSecurityConfiguration.java
+++ src/main/java/pl/jsystems/micro/configuration/WebSecurityConfiguration.java
@@ -1,21 +1,22 @@
 package pl.jsystems.micro.configuration;
 
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
-@Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests()     // poniższe adresy wymagają autoryzacji
-            .antMatchers("/api/json/users/**").hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
-            .anyRequest().permitAll()        // pozostałe żądania nie wymagają autoryzacji
-            .and()
-                .formLogin()
-            .and()
-                .httpBasic();
-    }
-}
+//@EnableWebSecurity
+//@Configuration
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeRequests()     // poniższe adresy wymagają autoryzacji
+//            .antMatchers("/api/json/roles/")
+//                .hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
+//            .anyRequest().permitAll()        // pozostałe żądania nie wymagają autoryzacji
+//            .and()
+//                .formLogin()
+//            .and()
+//                .httpBasic();
+//    }
+//}
--- src/main/java/pl/jsystems/micro/serivce/UserService.java
+++ src/main/java/pl/jsystems/micro/serivce/UserService.java
@@ -6,6 +6,9 @@ import pl.jsystems.micro.model.Role;
 import pl.jsystems.micro.model.User;
 import pl.jsystems.micro.repository.UserRepository;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -20,6 +23,21 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    public String passwordEncoder(String password)  {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            String output = "";
+            for (int i = 0; i < encodedhash.length; i++) {
+                output += String.format("%x", encodedhash[i]);
+            }
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 
     public User addUser(User user, Role role) {
         User userToSave = null;
@@ -28,6 +46,7 @@ public class UserService {
             Set<Role> roles = user.getRoles();      // do aktualnego zbioru ról dodaję role
             roles.add(role);
             userToSave.setRoles(roles);
+            userToSave.setPassword(passwordEncoder(userToSave.getPassword())); // szyfrowanie hasła
             userRepository.save(userToSave);
         }
         return userToSave;
